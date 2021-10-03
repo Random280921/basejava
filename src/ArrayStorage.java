38f5ef4c13ae3171b901a0060e2cf0c4d8f53349
Random280921 <ivanov_ka@list.ru>
2021-10-03 19:58:07 +0300

    Реализация delete(), get(), clear(); исправление ошибок по тестам

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,23 +5,46 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int size = this.size();
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume resume) {
-        int newId  = this.size();
-        newId = newId++;
-        if (storage.length == newId) {
-            System.out.print("Хранилище уже заполнено - резюме невозможно сохранить!");
-        } else {
-            storage[newId] = resume;
+        if (resume.uuid != null) {
+            int newId = this.size();
+            newId = newId++;
+            if (storage.length == newId) {
+                System.out.print("Хранилище уже заполнено - резюме невозможно сохранить!");
+            } else {
+                storage[newId] = resume;
+            }
         }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume findResume = null;
+        int i = 0;
+        while (storage[i] != null) {
+            if (uuid.equals(storage[i].uuid)) {
+                findResume = storage[i];
+                break;
+            }
+            i++;
+        }
+        return findResume;
     }
 
     void delete(String uuid) {
+        int size = this.size();
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                break;
+            }
+        }
     }
 
     /**
