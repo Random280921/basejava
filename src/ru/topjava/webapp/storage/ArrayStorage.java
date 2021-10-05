07c071d48f62f64a120302daf096d25144d429bc
Random280921 <ivanov_ka@list.ru>
2021-10-05 21:52:32 +0300

    Рефакторинг clear() и getAll() - замена на методы java.util.Arrays

--- src/ru/topjava/webapp/storage/ArrayStorage.java
+++ src/ru/topjava/webapp/storage/ArrayStorage.java
@@ -2,6 +2,8 @@ package ru.topjava.webapp.storage;
 
 import ru.topjava.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -10,9 +12,7 @@ public class ArrayStorage {
     private int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage,0,size,null);
         size = 0;
     }
 
@@ -96,11 +96,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] allResume = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            allResume[i] = storage[i];
-        }
-        return allResume;
+        return Arrays.copyOfRange(storage,0,size);
     }
 
     public int size() {
