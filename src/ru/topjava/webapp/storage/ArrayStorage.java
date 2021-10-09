3d05966ba8c951815b98ab81592d24d11b1d67a8
Random280921 <ivanov_ka@list.ru>
2021-10-09 19:00:28 +0300

    Подготовка проекта к выполнению ДЗ по уроку 3: * реализация equals() и hashcode() * static * сокращение большого описания курса до лаконичного

--- src/ru/topjava/webapp/storage/ArrayStorage.java
+++ src/ru/topjava/webapp/storage/ArrayStorage.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
+    private static final int STORAGE_LIMIT = 10_000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -17,7 +18,7 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (size >= storage.length) {
+        if (size >= STORAGE_LIMIT) {
             System.out.print("Хранилище уже заполнено - резюме невозможно сохранить!");
             return;
         }
