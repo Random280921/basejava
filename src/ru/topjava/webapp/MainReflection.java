0d8c927cdf0fd7609f81e2c1c22d75928340d503
Random280921 <ivanov_ka@list.ru>
2021-11-01 12:01:26 +0300

    ДЗ урок 4: reflect toString

--- src/ru/topjava/webapp/MainReflection.java
+++ src/ru/topjava/webapp/MainReflection.java
@@ -1,17 +1,20 @@
 package ru.topjava.webapp;
 
 import ru.topjava.webapp.model.Resume;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws Exception {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
-        System.out.println(r);
+        Method method = r.getClass().getMethod("toString");
+        System.out.println(method.invoke(r));
+//        System.out.println(r);
     }
 }
