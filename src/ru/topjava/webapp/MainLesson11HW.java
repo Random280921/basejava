dec45612f11d38dea127e912530e0138cea16f41
Random280921 <ivanov_ka@list.ru>
2021-12-28 17:31:38 +0300

    Урок 11: ДЗ Deadlock рефакторинг

--- src/ru/topjava/webapp/MainLesson11HW.java
+++ src/ru/topjava/webapp/MainLesson11HW.java
@@ -12,43 +12,12 @@ import java.lang.management.ThreadMXBean;
  * @version 1.0
  */
 public class MainLesson11HW {
-    public static final Object Lock1 = new Object();
-    public static final Object Lock2 = new Object();
+    public static final Lock Lock1 = new Lock("Lock1");
+    public static final Lock Lock2 = new Lock("Lock2");
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            final String threadName = Thread.currentThread().getName();
-            synchronized (Lock1) {
-                System.out.println(threadName + ": взяли Lock1");
-                System.out.println(threadName + ": ждём Lock2");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(threadName + ": пробуем взять Lock2");
-                synchronized (Lock2) {
-                    System.out.println(threadName + ": Lock1 и Lock2");
-                }
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            String threadName = Thread.currentThread().getName();
-            synchronized (Lock2) {
-                System.out.println(threadName + ": взяли Lock2");
-                System.out.println(threadName + ": ждём Lock1");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(threadName + ": пробуем взять Lock1");
-                synchronized (Lock1) {
-                    System.out.println(threadName + ": Lock2 и Lock1");
-                }
-            }
-        });
+        Thread thread1 = new Thread(() -> threadProcess(Lock1, Lock2));
+        Thread thread2 = new Thread(() -> threadProcess(Lock2, Lock1));
         thread1.start();
         thread2.start();
 
@@ -67,6 +36,31 @@ public class MainLesson11HW {
             for (ThreadInfo info : infos) {
                 if (info.getThreadName().equals(thread1.getName()) || info.getThreadName().equals(thread2.getName()))
                     System.out.println(info);
+            }
+        }
+    }
+
+    private static class Lock {
+        String name;
+
+        public Lock(String name) {
+            this.name = name;
+        }
+    }
+
+    private static void threadProcess(Lock l1, Lock l2) {
+        String threadName = Thread.currentThread().getName();
+        synchronized (l1) {
+            System.out.println(threadName + ": взяли " + l1.name);
+            System.out.println(threadName + ": ждём " + l2.name);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(threadName + ": пробуем взять " + l2.name);
+            synchronized (l2) {
+                System.out.println(threadName + ": " + l1.name + " и " + l2.name);
             }
         }
     }
