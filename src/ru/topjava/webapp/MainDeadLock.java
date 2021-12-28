2ee1166dc988b594d259127466104bd0358ec013
Random280921 <ivanov_ka@list.ru>
2021-12-28 19:08:01 +0300

    Урок 11: ДЗ Deadlock рефакторинг

--- src/ru/topjava/webapp/MainDeadLock.java
+++ src/ru/topjava/webapp/MainDeadLock.java
@@ -11,15 +11,13 @@ import java.lang.management.ThreadMXBean;
  * created by 28.12.2021 15:30
  * @version 1.0
  */
-public class MainLesson11HW {
-    public static final Lock Lock1 = new Lock("Lock1");
-    public static final Lock Lock2 = new Lock("Lock2");
-
+public class MainDeadLock {
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> threadProcess(Lock1, Lock2));
-        Thread thread2 = new Thread(() -> threadProcess(Lock2, Lock1));
-        thread1.start();
-        thread2.start();
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+
+        new Thread(() -> threadProcess(lock1, lock2)).start();
+        new Thread(() -> threadProcess(lock2, lock1)).start();
 
         try {
             Thread.sleep(300);
@@ -34,33 +32,25 @@ public class MainLesson11HW {
         if (threadIds != null) {
             ThreadInfo[] infos = threadMXBean.getThreadInfo(threadIds);
             for (ThreadInfo info : infos) {
-                if (info.getThreadName().equals(thread1.getName()) || info.getThreadName().equals(thread2.getName()))
+                if (info.getThreadName().contains("Thread"))
                     System.out.println(info);
             }
         }
     }
 
-    private static class Lock {
-        String name;
-
-        private Lock(String name) {
-            this.name = name;
-        }
-    }
-
-    private static void threadProcess(Lock l1, Lock l2) {
+    private static void threadProcess(String l1, String l2) {
         String threadName = Thread.currentThread().getName();
         synchronized (l1) {
-            System.out.println(threadName + ": взяли " + l1.name);
-            System.out.println(threadName + ": ждём " + l2.name);
+            System.out.println(threadName + ": взяли " + l1);
+            System.out.println(threadName + ": ждём " + l2);
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(threadName + ": пробуем взять " + l2.name);
+            System.out.println(threadName + ": пробуем взять " + l2);
             synchronized (l2) {
-                System.out.println(threadName + ": " + l1.name + " и " + l2.name);
+                System.out.println(threadName + ": " + l1 + " и " + l2);
             }
         }
     }
