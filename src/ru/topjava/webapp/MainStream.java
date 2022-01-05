ebae6242b9936373dafc1e409ed498edcf7c3be8
Random280921 <ivanov_ka@list.ru>
2022-01-05 12:05:11 +0300

    Урок 12: ДЗ Stream - Работа по замечаниям 1

--- src/ru/topjava/webapp/MainStream.java
+++ src/ru/topjava/webapp/MainStream.java
@@ -1,10 +1,7 @@
 package ru.topjava.webapp;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 /**
@@ -19,35 +16,24 @@ public class MainStream {
         int[] arr = {2, 3, 6, 6, 4, 6, 6, 8, 2, 3};
         System.out.println(minValue(arr));
 
-        List<Integer> beginning = Arrays.asList(2, 6, 4, 7, 8, 5, 9);
+        List<Integer> beginning = Arrays.asList(1, 6, 4, 7, 8, 5, 9);
         System.out.println("Сумма элементов: " + beginning.stream().reduce(0, Integer::sum));
         List<Integer> ending = oddOrEven(beginning);
         for (Integer integer : ending) System.out.println(integer);
     }
 
     private static int minValue(int[] values) {
-        AtomicInteger cnt = new AtomicInteger(-1);
         return Arrays.stream(values)
-                .boxed()
                 .distinct()
-                .sorted(Comparator.reverseOrder())
-                .map(x -> x * (Math.pow(10, cnt.incrementAndGet())))
-                .mapToInt(Double::intValue)
-                .sum();
+                .sorted()
+                .reduce(0, (abs, x) -> abs * 10 + x);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        AtomicReference<Integer> Sum = new AtomicReference<>(0);
+        int sum = integers.stream().reduce(0, Integer::sum);
         return integers
                 .stream()
-                .peek(x -> Sum.updateAndGet(v -> v + x))
-                .collect(Collectors
-                        .collectingAndThen(
-                                Collectors.toList(),
-                                c -> c.stream()
-                                        .filter(x -> (Sum.get() % 2 == 0 && x % 2 == 1) || (Sum.get() % 2 == 1 && x % 2 == 0))
-                                        .collect(Collectors.toList())
-                        )
-                );
+                .filter(x -> (sum % 2 != x % 2))
+                .collect(Collectors.toList());
     }
 }
