8fc5c8640e45b410b52b3591a4d341b09e0bc681
Random280921 <ivanov_ka@list.ru>
2022-01-06 23:45:02 +0300

    Урок 13: ДЗ рефакторинг через SqlHelper

---
+++ src/ru/javaonline/basejava/sql/SqlHelper.java
@@ -0,0 +1,31 @@
+package ru.javaonline.basejava.sql;
+
+import ru.javaonline.basejava.exception.StorageException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Класс - утилита для обслуживания SQL
+ *
+ * @author KAIvanov
+ * created by 06.01.2022 21:50
+ * @version 1.0
+ */
+public class SqlHelper {
+
+    @FunctionalInterface
+    public interface SqlSupplier<T> {
+        T get(PreparedStatement ps) throws SQLException;
+    }
+
+    public <T> T sqlExecutor(String sql, ConnectionFactory connectionFactory, SqlSupplier<T> sqlSupplier) {
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            return sqlSupplier.get(ps);
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+}
--- src/ru/javaonline/basejava/storage/SqlStorage.java
+++ src/ru/javaonline/basejava/storage/SqlStorage.java
@@ -1,11 +1,12 @@
 package ru.javaonline.basejava.storage;
 
 import ru.javaonline.basejava.exception.NotExistStorageException;
-import ru.javaonline.basejava.exception.StorageException;
 import ru.javaonline.basejava.model.Resume;
 import ru.javaonline.basejava.sql.ConnectionFactory;
+import ru.javaonline.basejava.sql.SqlHelper;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.List;
 
 /**
@@ -16,32 +17,48 @@ import java.util.List;
  * @version 1.0
  */
 public class SqlStorage implements Storage {
-    public final ConnectionFactory connectionFactory;
+    private final ConnectionFactory connectionFactory;
+    private final SqlHelper sqlHelper = new SqlHelper();
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
         connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
+
     @Override
     public void clear() {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("DELETE FROM resume")) {
+        String sql = "DELETE FROM resume";
+        sqlHelper.sqlExecutor(sql, connectionFactory, ps -> {
             ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+            return null;
+        });
     }
+//        try (Connection conn = connectionFactory.getConnection();
+//             PreparedStatement ps = conn.prepareStatement("DELETE FROM resume")) {
+//            ps.execute();
+//        } catch (SQLException e) {
+//            throw new StorageException(e);
+//        }
+//    }
 
     @Override
     public void save(Resume resume) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
+        String sql = "INSERT INTO resume (uuid, full_name) VALUES (?,?)";
+        sqlHelper.sqlExecutor(sql, connectionFactory, ps -> {
             ps.setString(1, resume.getUuid());
             ps.setString(2, resume.getFullName());
             ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+            return null;
+        });
     }
+//        try (Connection conn = connectionFactory.getConnection();
+//             PreparedStatement ps = conn.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
+//            ps.setString(1, resume.getUuid());
+//            ps.setString(2, resume.getFullName());
+//            ps.execute();
+//        } catch (SQLException e) {
+//            throw new StorageException(e);
+//        }
+//}
 
     @Override
     public void update(Resume resume) {
@@ -50,18 +67,28 @@ public class SqlStorage implements Storage {
 
     @Override
     public Resume get(String uuid) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid =?")) {
+        String sql = "SELECT * FROM resume r WHERE r.uuid =?";
+        return sqlHelper.sqlExecutor(sql, connectionFactory, ps -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
             return new Resume(uuid, rs.getString("full_name"));
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        });
     }
+//        try (Connection conn = connectionFactory.getConnection();
+//             PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid =?")) {
+//            ps.setString(1, uuid);
+//            ResultSet rs = ps.executeQuery();
+//            if (!rs.next()) {
+//                throw new NotExistStorageException(uuid);
+//            }
+//            return new Resume(uuid, rs.getString("full_name"));
+//        } catch (SQLException e) {
+//            throw new StorageException(e);
+//        }
+//    }
 
     @Override
     public void delete(String uuid) {
