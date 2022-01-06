7f3b4cfadf9756c1640479a81b234bf5f3fe7e37
Random280921 <ivanov_ka@list.ru>
2022-01-06 17:44:13 +0300

    Урок 13: config db

---
+++ config/init_db.sql
@@ -0,0 +1,33 @@
+drop table if exists contact;
+drop table if exists resume;
+
+create table resume
+(
+    uuid      char(36) not null
+        constraint resume_pk
+            primary key,
+    full_name text     not null
+);
+
+alter table resume
+    owner to postgres;
+
+create table contact
+(
+    id          serial
+        constraint contact_pk
+            primary key,
+    resume_uuid char(36)    not null
+        constraint contact_resume_uuid_fk
+            references resume (uuid)
+            on delete cascade,
+    type        varchar(32) not null,
+    value       text        not null,
+    url         text
+);
+
+create unique index contact_resume_uuid_type_uq
+    on contact (resume_uuid, type);
+
+alter table contact
+    owner to postgres;
